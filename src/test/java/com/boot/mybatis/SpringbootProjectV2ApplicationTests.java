3675a4a0795c2f4be2f4347a50996bcf810c8191
SUJINJEONG012 <peekaboo32@naver.com>
2023-09-17 00:55:43 +0900

    스프링부트와 마이바티스 공부 다시

---
+++ src/test/java/com/boot/mybatis/SpringbootProjectV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProjectV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
