4e8dd5487390e3f77ad41ff8fab3a00c5249a449
angela <peekaboo32@naver.com>
2023-09-18 13:11:00 +0900

    sqlsesion 테스트

---
+++ src/test/java/com/boot/mybatis/BoardApplicationTests.java
@@ -0,0 +1,25 @@
+package com.boot.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BoardApplicationTests {
+
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Test
+	public void contextLoads() {
+		
+	}
+	
+	@Test
+	public void testSqlSession() throws Exception{
+		System.out.println(sqlSession.toString());
+	}
+	
+	
+}
