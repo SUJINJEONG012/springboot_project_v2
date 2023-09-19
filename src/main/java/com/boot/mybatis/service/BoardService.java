d8d7e84e8b8bac65fc0bda78dd216ade7607e19e
angela <peekaboo32@naver.com>
2023-09-19 13:05:05 +0900

    게시글 리스트에 제목에 링크 걸기 상세로 가기

--- src/main/java/com/boot/mybatis/service/BoardService.java
+++ src/main/java/com/boot/mybatis/service/BoardService.java
@@ -6,7 +6,12 @@ import com.boot.mybatis.dto.BoardDto;
 
 public interface BoardService {
 
+	// 게시물 리스트
 	List<BoardDto> selectBoardList() throws Exception;
 	
+	// 게시물 등록
 	public void insertBoard(BoardDto boardDto) throws Exception;
+	
+	// 게시글 조회, 게시글 조회수 증가
+	
 }
