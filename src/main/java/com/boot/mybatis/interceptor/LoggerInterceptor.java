9d892bb2cd777a3809e2f43701b8f708c7947476
SUJINJEONG012 <peekaboo32@naver.com>
2023-09-24 01:39:01 +0900

    컨트롤러 실행되기전과 컨트롤러 실행되고 난 후 로그 찍히는거 확인

--- src/main/java/com/boot/mybatis/interceptor/LoggerInterceptor.java
+++ src/main/java/com/boot/mybatis/interceptor/LoggerInterceptor.java
@@ -3,8 +3,8 @@ package com.boot.mybatis.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.catalina.connector.Request;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,7 +20,7 @@ public class LoggerInterceptor implements HandlerInterceptor {
 	}
 	
 	// 컨트롤러 실행된 후 수행
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 		log.info("===============end===============");
 		
 	}
