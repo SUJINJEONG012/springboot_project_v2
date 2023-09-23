e1063fd3c7b888c64b36bbb3a75068c07ba6e637
SUJINJEONG012 <peekaboo32@naver.com>
2023-09-24 01:38:50 +0900

    컨트롤러 실행되기전과 컨트롤러 실행되고 난 후 로그 찍히는거 확인

--- src/main/java/com/boot/mybatis/interceptor/LoggerInterceptor.java
+++ src/main/java/com/boot/mybatis/interceptor/LoggerInterceptor.java
@@ -14,14 +14,14 @@ public class LoggerInterceptor implements HandlerInterceptor {
 	
 	// 컨트롤러 실행되기 전 수행
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-		log.debug("===============start===============");
-		log.debug("Request URL \t:  " + request.getRequestURI());
+		log.info("===============start===============");
+		log.info("Request URL \t:  " + request.getRequestURI());
 		return true;
 	}
 	
 	// 컨트롤러 실행된 후 수행
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-		log.debug("===============end===============");
+		log.info("===============end===============");
 		
 	}
 	
