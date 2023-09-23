bb47b8ae44822726cfa1d3424d0eb559767cf935
SUJINJEONG012 <peekaboo32@naver.com>
2023-09-23 21:36:34 +0900

    인터셉터 만든거  빈으로 등록

---
+++ src/main/java/com/boot/mybatis/configuration/WebMvcConfiguration.java
@@ -0,0 +1,16 @@
+package com.boot.mybatis.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.boot.mybatis.interceptor.LoggerInterceptor;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+	
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LoggerInterceptor());
+	}
+}
--- src/main/java/com/boot/mybatis/interceptor/LoggerInterceptor.java
+++ src/main/java/com/boot/mybatis/interceptor/LoggerInterceptor.java
@@ -1,9 +1,28 @@
 package com.boot.mybatis.interceptor;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.connector.Request;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-@Slfj4
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class LoggerInterceptor implements HandlerInterceptor {
 
+	
+	// 컨트롤러 실행되기 전 수행
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		log.debug("===============start===============");
+		log.debug("Request URL \t:  " + request.getRequestURI());
+		return true;
+	}
+	
+	// 컨트롤러 실행된 후 수행
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		log.debug("===============end===============");
+		
+	}
 	
 }
