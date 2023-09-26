ac27b2ff025fee0be4768cfe66e9e998db652edc
SUJINJEONG012 <peekaboo32@naver.com>
2023-09-26 22:43:37 +0900

    스프링부트의 기본 파일설정이 자동설정 해제 설정하기

--- src/main/java/com/boot/mybatis/SpringbootProjectV2Application.java
+++ src/main/java/com/boot/mybatis/SpringbootProjectV2Application.java
@@ -2,12 +2,15 @@ package com.boot.mybatis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={MultipartAutoConfiguration.class})
 public class SpringbootProjectV2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootProjectV2Application.class, args);
 	}
+	
 
+	
 }
