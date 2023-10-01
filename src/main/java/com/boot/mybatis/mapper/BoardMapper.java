6ef2fb45a0e6e0ee214fcaa3c1a7876fc7af45bb
SUJINJEONG012 <peekaboo32@naver.com>
2023-10-02 01:51:49 +0900

    insertBoardFileList 추가, 리스트로 FileUtils에 만든 BoardFileDto사용

--- src/main/java/com/boot/mybatis/mapper/BoardMapper.java
+++ src/main/java/com/boot/mybatis/mapper/BoardMapper.java
@@ -5,17 +5,20 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.boot.mybatis.dto.BoardDto;
+import com.boot.mybatis.dto.BoardFileDto;
 
 @Mapper
 public interface BoardMapper {
 
 	List<BoardDto> selectBoardList() throws Exception;
 	public void insertBoard(BoardDto boardDto) throws Exception;
+	public void insertBoardFileList(List<BoardFileDto> list) throws Exception;
 	public void updateHitCount(int boardIdx) throws Exception;
 	BoardDto selectBoardDetail(int boardIdx) throws Exception;
 	
 	public void updateBoard(BoardDto boardDto) throws Exception;
 	public void deleteBoard(int boardIdx) throws Exception;
+	
 	
 	
 }
