404eeb259608141513e0598f4a091978f7259268
SUJINJEONG012 <peekaboo32@naver.com>
2023-10-01 18:04:06 +0900

    파일처리를 위한 클래스 생성하기

---
+++ src/main/java/com/boot/mybatis/common/FileItils.java
@@ -0,0 +1,23 @@
+package com.boot.mybatis.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.boot.mybatis.dto.BoardFileDto;
+
+@Component
+public class FileItils {
+
+	public List<BoardFileDto> parseFileInfo(int boardIdx, MultipartHttpServletRequest multipartHttpServletRequest) throws Exception{
+		if() {
+			
+		}
+		
+		List<BoardFileDto> fileList = new ArrayList<>();
+		
+		return null;
+	}
+}
