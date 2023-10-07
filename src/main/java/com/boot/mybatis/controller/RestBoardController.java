b41530d7b35836f35b8f872bb2b0b882a9a934c9
SUJINJEONG012 <peekaboo32@naver.com>
2023-10-07 16:06:13 +0900

    restcontroller 만들기

---
+++ src/main/java/com/boot/mybatis/controller/RestBoardController.java
@@ -0,0 +1,15 @@
+package com.boot.mybatis.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.boot.mybatis.service.BoardService;
+
+@Controller
+public class RestBoardController {
+
+	@Autowired
+	private BoardService boardService;
+	
+	@Re
+}
